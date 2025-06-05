d20d0f42107caf9bd0fac622cbfd68d121853be4
ahmadfadliakbar <afka.akbar@gmail.com>
2025-06-05 21:22:27 +0700

    Latihan: Memperbaiki code smell yang terdeteksi SonarCloud

--- script.js
+++ script.js
@@ -48,8 +48,3 @@ function calculateResult() {
 if (typeof module !== 'undefined' && module.exports) { 
     module.exports = { performCalculation }; 
 }
-
-function clearDisplay() { 
-display.value = ''; 
-let unusedVariableForDemo = "hello"; // Variabel tidak digunakan 
-} 
